524ce37abf469bba52f3f6bde34a644f9273a858
mohamed nousier <73021351+Mohamed-Nousier@users.noreply.github.com>
2023-05-25 05:59:12 +0300

    Update EXTI_program.c

--- 01 MCAL/03 EXTI/Src/EXTI_program.c
+++ 01 MCAL/03 EXTI/Src/EXTI_program.c
@@ -29,37 +29,114 @@ void		EXTI_voidInit( u8 port , u8 line_num)
             #define		AFIO_EXTICR4			*((u32 *)(AFIO_BASE_ADD+0x14))
 		#endif
 	if(line_num <16 )
-	{
-		switch(port)
 		{
-		case GPIO_PORTA :
-			if(line_num >= 0 && line_num <= 3){AFIO_EXTICR1 = (0000<<line_num);};
-			if(line_num >= 4 && line_num <= 7){AFIO_EXTICR2 = (0000<<line_num);};
-			if(line_num >= 4 && line_num <= 7){AFIO_EXTICR3 = (0000<<line_num);};
-			if(line_num >= 4 && line_num <= 7){AFIO_EXTICR4 = (0000<<line_num);}break;
-		case GPIO_PORTB :
-			if(line_num >= 0 && line_num <= 3){AFIO_EXTICR1 = (0001<<line_num);};
-			if(line_num >= 4 && line_num <= 7){AFIO_EXTICR2 = (0001<<line_num);};
-			if(line_num >= 4 && line_num <= 7){AFIO_EXTICR3 = (0001<<line_num);};
-			if(line_num >= 4 && line_num <= 7){AFIO_EXTICR4 = (0001<<line_num);}break;
+			switch(port)
+			{
+			case GPIO_PORTA :
+				if(line_num >= 0 && line_num <= 3){AFIO_EXTICR1 = (0000<<line_num);};
+				if(line_num >= 4 && line_num <= 7){AFIO_EXTICR2 = (0000<<line_num);};
+				if(line_num >= 8 && line_num <= 11){AFIO_EXTICR3 = (0000<<line_num);};
+				if(line_num >= 12 && line_num <= 15){AFIO_EXTICR4 = (0000<<line_num);}break;
 
-		case GPIO_PORTC :
-			if(line_num >= 0 && line_num <= 3){AFIO_EXTICR1 = (0010<<line_num);};
-			if(line_num >= 4 && line_num <= 7){AFIO_EXTICR2 = (0010<<line_num);};
-			if(line_num >= 4 && line_num <= 7){AFIO_EXTICR3 = (0010<<line_num);};
-			if(line_num >= 4 && line_num <= 7){AFIO_EXTICR4 = (0010<<line_num);}break;
+			case GPIO_PORTB :
+				if(line_num >= 0 && line_num <= 3)
+				{
+					switch(line_num)
+					{
+						case 0:
+							AFIO_EXTICR1 |= (0b0001 << 0);break;
+						case 1:
+							AFIO_EXTICR1 |= (0b0001 << 4);break;
+						case 2:
+							AFIO_EXTICR1 |= (0b0001 << 8);break;
+						case 3:
+							AFIO_EXTICR1 |= (0b0001 << 12);break;
 
-		}
+
+					}
+				}
+
+				if(line_num >= 4 && line_num <= 7)
+				{
+					switch(line_num)
+					{
+						case 4:
+							AFIO_EXTICR2 |= (0b0001 << 0);break;
+						case 5:
+							AFIO_EXTICR2 |= (0b0001 << 4);break;
+						case 6:
+							AFIO_EXTICR2 |= (0b0001 << 8);break;
+						case 7:
+							AFIO_EXTICR2 |= (0b0001 << 12);break;
+
+
+					}
+
+				}
+
+				if(line_num >= 8 && line_num <= 11)
+				{
+					switch(line_num)
+					{
+						case 8:
+							AFIO_EXTICR3 |= (0b0001 << 0);break;
+						case 9:
+							AFIO_EXTICR3 |= (0b0001 << 4);break;
+						case 10:
+							AFIO_EXTICR3 |= (0b0001 << 8);break;
+						case 11:
+							AFIO_EXTICR3 |= (0b0001 << 12);break;
+
+
+					}
+
+				}
+				if(line_num >= 12 && line_num <= 15)
+				{
+					switch(line_num)
+					{
+						case 12:
+							AFIO_EXTICR4 |= (0b0001 << 0);break;
+						case 13:
+							AFIO_EXTICR4 |= (0b0001 << 4);break;
+						case 14:
+							AFIO_EXTICR4 |= (0b0001 << 8);break;
+						case 15:
+							AFIO_EXTICR4 |= (0b0001 << 12);break;
+
+
+					}
+
+				}break;
+
+			case GPIO_PORTC :
+				if(line_num >= 12 && line_num <= 15)
+				{
+					switch (line_num)
+					{
+						case 12:
+							AFIO_EXTICR4 |= (0b0010 << 0);break;
+						case 13:
+							AFIO_EXTICR4 |= (0b0010 << 4);break;
+						case 14:
+							AFIO_EXTICR4 |= (0b0010 << 8);break;
+						case 15:
+							AFIO_EXTICR4 |= (0b0010 << 12);break;
+
+					}
+				}break;
+
+			}
 
 		/*************************/
 		//Sense mode config
-		EXTI_voidChangeSenseMode(line_num,RISING_EDGE );
+		//EXTI_voidChangeSenseMode(line_num,RISING_EDGE );
 		/*************************/
 
 		/**************************/
 		//disable interrupt & clear pending flag
 		CLR_BIT(EXTI -> IMR ,line_num);
-		SET_BIT(EXTI->PR ,line_num);
+		//SET_BIT(EXTI->PR ,line_num);
 		/****************************/
 	}
 }
